31a4ecd0666a919075b34d73547d23c4c88747b3
Marty Martin <marty@locomotive.agency>
2026-09-02 16:34:57 -0400

    feat: allow reference works source class

---
+++ src/author_library/storage/migrations/014_reference_source_class.sql
@@ -0,0 +1,7 @@
+-- 014_reference_source_class.sql
+-- Add 'reference' to source_class CHECK constraint on works table.
+
+-- Drop and recreate the CHECK constraint to include 'reference'
+ALTER TABLE works DROP CONSTRAINT IF EXISTS works_source_class_check;
+ALTER TABLE works ADD CONSTRAINT works_source_class_check
+    CHECK (source_class IN ('primary', 'secondary', 'contextual', 'tertiary', 'personal', 'reference'));
